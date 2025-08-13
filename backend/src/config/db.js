17666361ed2f584136029eb457a1f0ba8f297ee6
Miguel Rodriguez <wmrm1@hotmail.com>
2025-08-13 16:40:18 -0500

    Delete backend directory
    Se modifican completamente los archivos, aquellos cargados anteriormente no correspondian al proyecto en desarrollo.

--- backend/src/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-export const connectDB = async () => {
-  const uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/solicitudes';
-  await mongoose.connect(uri);
-  console.log('✅ MongoDB conectado');
-};
